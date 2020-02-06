ce19b24075f7ab56a51d33e96ae29bbf981ae5b7
Herman Singh <hermansingh1993@gmail.com>
2020-02-06 21:37:05 +0100

    added more

---
+++ Part 2/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+            count = count + 1;
+            sum = sum + number;
+        }
+        System.out.println("Average of the numbers: " + ((sum + 0.0) / count));
+    }
+}
+
