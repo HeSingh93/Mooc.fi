217997b95c8ecbf087c0dbe4e880fcb058469ed1
Herman Singh <hermansingh1993@gmail.com>
2020-02-07 15:24:24 +0100

    added exercises

---
+++ Part 2/part02-Part02_28.Word/Word.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        word();
+    }
+
+    static String word() {
+        return "WORD!";
+    }
+}
